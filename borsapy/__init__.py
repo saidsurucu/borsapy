34a5b4b289872642cf04bfd1878c5dbb32e83ce8
saidsurucu <saidsrc@gmail.com>
2026-01-06 20:10:26 +0300

    Bump version to 0.2.0

--- borsapy/__init__.py
+++ borsapy/__init__.py
@@ -83,7 +83,7 @@ from borsapy.screener import Screener, screen_stocks, screener_criteria, sectors
 from borsapy.ticker import Ticker
 from borsapy.viop import VIOP
 
-__version__ = "0.1.0"
+__version__ = "0.2.0"
 __author__ = "Said Surucu"
 
 __all__ = [
